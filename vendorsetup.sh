85066972e9b8ad3b0007b854bc436c170193bfbb
Teenarp2003 <praneet.guharoy@gmail.com>
2022-01-17 13:07:25 +0530

    sm6250-common: Switch to proton clang

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+rm -rf hardware/xiaomi
+git clone https://github.com/ArrowOS-Devices/android_hardware_xiaomi.git -b arrow-12.0 hardware/xiaomi
+git clone https://github.com/ArrowOS-Devices/android_packages_apps_GCamGOPrebuilt.git -b arrow-12.0 packages/apps/GCamGOPrebuilt
+git clone https://github.com/ArrowOS-Devices/android_device_xiaomi_extras.git -b arrow-12.0 device/xiaomi/extras
+git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/clang-proton
+#git clone https://github.com/SonalSingh18/vendor_xiaomi_miuicamera.git -b master vendor/xiaomi/miuicamera
