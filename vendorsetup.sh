99714d2933ecfaa221ef4755823ac184a46f0115
Teenarp2003 <praneet.guharoy@gmail.com>
2022-04-30 13:58:32 +0530

    Revert "sm6250-common: Switch to proton clang"
    This reverts commit 85066972e9b8ad3b0007b854bc436c170193bfbb.

--- vendorsetup.sh
+++
@@ -1,2 +0,0 @@
-git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/clang-proton
-#git clone https://github.com/SonalSingh18/vendor_xiaomi_miuicamera.git -b master vendor/xiaomi/miuicamera
